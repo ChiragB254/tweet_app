572ff4932f60490cb9db0579b78557b73c026370
Chirag Bansal <chiragbansal254@gmail.com>
2024-04-07 17:02:16 -0400

    07-04-2024_Commit

--- app.py
+++ app.py
@@ -27,7 +27,19 @@ st.set_page_config(page_title = "Disaster Detection App", page_icon = "Images/lo
 st.title('Disaster Detection App')
 
 #  Sidebar for the WebApp, where the user can select the model which they want to use for Prediction
-menu = st.sidebar.radio("Menu",["Home","XGBoost","Random Forest","Logistic Regression","KNN","Naive Bayes","Decision Tree","About"])
+menu = st.sidebar.radio("Menu",["Home",
+                                "Ada Boost",
+                                "Bernoulli Naive Bayes",
+                                "Cat Boost",
+                                "Decision Tree",
+                                "Extra Tree",
+                                "Gradient Boosting",
+                                "K Nearest Neighbors",
+                                "Logistic Regression",
+                                "Random Forest",
+                                "Ridge Classifier",
+                                "XG Boost",
+                                "About"])
 
 #  Conditional statements for the WebApp sidebar based on whether a new model or an existing model is selected 
 if menu=="Home":
@@ -35,14 +47,13 @@ if menu=="Home":
     st.write("Welcome to the Disaster Detection App")
     st.write("This app is designed to predict whether a tweet is about a disaster or not.")
     st.write("Please select a model from the sidebar to get started.")
-
-elif menu=="XGBoost":
-    # Load the trained XGBoost model
-
-    with open('trained_models/xgboost_model.pickle', 'rb') as f:
+# ========================================================================================================
+elif menu=="Ada Boost":
+    # Load the trained AdaBoost model
+    with open('trained_models/AdaBoostClassifier.pickle', 'rb') as f:
         loaded_model = pickle.load(f)
     # Display the model name
-    st.header('XGBoost Model')
+    st.header('Ada Boost Model')
     # Text area for the user to enter the tweet
     tweet = st.text_area('Enter Tweet')
     # Button to predict the tweet
@@ -51,12 +62,13 @@ elif menu=="XGBoost":
             prediction(tweet, loaded_model)
         else:
             st.error('Please enter a tweet')
-elif menu=="Random Forest":
-    # Load the Random Forest Classifier model
-    with open('trained_models/randomforest_model.pickle', 'rb') as f:
+# ========================================================================================================
+elif menu=="Bernoulli Naive Bayes":
+    # Load the trained Bernoulli Naive Bayes model
+    with open('trained_models/BernoulliNB.pickle', 'rb') as f:
         loaded_model = pickle.load(f)
     # Display the model name
-    st.header('Random Forest Model')
+    st.header('Bernoulli Naive Bayes Model')
     # Text area for the user to enter the tweet
     tweet = st.text_area('Enter Tweet')
     # Button to predict the tweet
@@ -65,12 +77,13 @@ elif menu=="Random Forest":
             prediction(tweet, loaded_model)
         else:
             st.error('Please enter a tweet')
-elif menu=="Logistic Regression":
-    # Load the Logistic Regression model
-    with open('trained_models/logistic_model.pickle', 'rb') as f:
+# ========================================================================================================
+elif menu=="Cat Boost":
+    # Load the trained Cat Boost model
+    with open('trained_models/CatBoostClassifier.pickle', 'rb') as f:
         loaded_model = pickle.load(f)
     # Display the model name
-    st.header('Logistic Regression Model')
+    st.header('Cat Boost Classifier Model')
     # Text area for the user to enter the tweet
     tweet = st.text_area('Enter Tweet')
     # Button to predict the tweet
@@ -79,38 +92,10 @@ elif menu=="Logistic Regression":
             prediction(tweet, loaded_model)
         else:
             st.error('Please enter a tweet')
-elif menu=="KNN":
-    # Load the KNN model
-    with open('trained_models/knn_model.pickle', 'rb') as f:
-        loaded_model = pickle.load(f)
-    # Display the model name
-    st.header('KNN Model')
-    # Text area for the user to enter the tweet
-    tweet = st.text_area('Enter Tweet')
-    # Button to predict the tweet
-    if st.button('Predict'):
-        if tweet:
-            prediction(tweet, loaded_model)
-        else:
-            st.error('Please enter a tweet')     
-elif menu=="Naive Bayes":
-    # Load the Naive Bayes model
-    with open('trained_models/nb_model.pickle', 'rb') as f:
-        loaded_model = pickle.load(f)
-    # Display the model name
-    st.header('Navie Bayes Model')
-    # Text area for the user to enter the tweet
-    tweet = st.text_area('Enter Tweet')
-    # Button to predict the tweet
-    if st.button('Predict'):
-        if tweet:
-            prediction(tweet, loaded_model)
-        else:
-            st.error('Please enter a tweet')
-
+# ========================================================================================================
 elif menu=="Decision Tree":
     # Load the Decision Tree model
-    with open('trained_models/dt_model.pickle', 'rb') as f:
+    with open('trained_models/DecisionTreeClassifier.pickle', 'rb') as f:
         loaded_model = pickle.load(f)
     # Display the model name
     st.header('Decision Tree Model')
@@ -122,7 +107,112 @@ elif menu=="Decision Tree":
             prediction(tweet, loaded_model)
         else:
             st.error('Please enter a tweet')
-
+# ========================================================================================================
+elif menu=="Extra Tree":
+    # Load the Extra Tree model
+    with open('trained_models/ExtraTreeClassifier.pickle', 'rb') as f:
+        loaded_model = pickle.load(f)
+    # Display the model name
+    st.header('Extra Tree Classifier Model')
+    # Text area for the user to enter the tweet
+    tweet = st.text_area('Enter Tweet')
+    # Button to predict the tweet
+    if st.button('Predict'):
+        if tweet:
+            prediction(tweet, loaded_model)
+        else:
+            st.error('Please enter a tweet')
+# ========================================================================================================
+elif menu=="Gradient Boosting":
+    # Load the Gradient Boosting model
+    with open('trained_models/GradientBoostingClassifier.pickle', 'rb') as f:
+        loaded_model = pickle.load(f)
+    # Display the model name
+    st.header('Gradient Boosting Model')
+    # Text area for the user to enter the tweet
+    tweet = st.text_area('Enter Tweet')
+    # Button to predict the tweet
+    if st.button('Predict'):
+        if tweet:
+            prediction(tweet, loaded_model)
+        else:
+            st.error('Please enter a tweet')
+# ========================================================================================================
+elif menu=="K Nearest Neighbors":
+    # Load the KNN model
+    with open('trained_models/KNeighborsClassifier.pickle', 'rb') as f:
+        loaded_model = pickle.load(f)
+    # Display the model name
+    st.header('K Nearest Neighbors Classifier Model')
+    # Text area for the user to enter the tweet
+    tweet = st.text_area('Enter Tweet')
+    # Button to predict the tweet
+    if st.button('Predict'):
+        if tweet:
+            prediction(tweet, loaded_model)
+        else:
+            st.error('Please enter a tweet') 
+# ========================================================================================================
+elif menu=="Logistic Regression":
+    # Load the Logistic Regression model
+    with open('trained_models/LogisticRegressionCV.pickle', 'rb') as f:
+        loaded_model = pickle.load(f)
+    # Display the model name
+    st.header('Logistic Regression Model')
+    # Text area for the user to enter the tweet
+    tweet = st.text_area('Enter Tweet')
+    # Button to predict the tweet
+    if st.button('Predict'):
+        if tweet:
+            prediction(tweet, loaded_model)
+        else:
+            st.error('Please enter a tweet') 
+# ========================================================================================================
+elif menu=="Random Forest":
+    # Load the Random Forest Classifier model
+    with open('trained_models/RandomForestClassifier.pickle', 'rb') as f:
+        loaded_model = pickle.load(f)
+    # Display the model name
+    st.header('Random Forest Model')
+    # Text area for the user to enter the tweet
+    tweet = st.text_area('Enter Tweet')
+    # Button to predict the tweet
+    if st.button('Predict'):
+        if tweet:
+            prediction(tweet, loaded_model)
+        else:
+            st.error('Please enter a tweet')
+# ========================================================================================================
+elif menu=="Ridge Classifier":
+    # Load the Ridge Classifier model
+    with open('trained_models/RidgeClassifierCV.pickle', 'rb') as f:
+        loaded_model = pickle.load(f)
+    # Display the model name
+    st.header('Ridge Classifier Model')
+    # Text area for the user to enter the tweet
+    tweet = st.text_area('Enter Tweet')
+    # Button to predict the tweet
+    if st.button('Predict'):
+        if tweet:
+            prediction(tweet, loaded_model)
+        else:
+            st.error('Please enter a tweet')
+# ========================================================================================================
+elif menu=="XG Boost":
+    # Load the XGBoost classifier model
+    with open('trained_models/XGBClassifier.pickle', 'rb') as f:
+        loaded_model = pickle.load(f)
+    # Display the model name
+    st.header('XGBoost Classifier Model')
+    # Text area for the user to enter the tweet
+    tweet = st.text_area('Enter Tweet')
+    # Button to predict the tweet
+    if st.button('Predict'):
+        if tweet:
+            prediction(tweet, loaded_model)
+        else:
+            st.error('Please enter a tweet')
+# ========================================================================================================
 elif menu=="About":
     # Display the about us page
     with open('html_pages/About_us.html', 'rb') as f:
